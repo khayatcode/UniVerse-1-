c361276ce40ac6cdb570b4a18d99939dee30ffcf
khayatcode <a.khayatcode0103@gmail.com>
2023-08-13 17:39:57 -0600

    netlify

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ from flask_app.controllers import users, posts, comments
 from flask_cors import CORS
 
 
-CORS(app, support_credentials=True)
+# CORS(app, support_credentials=True)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, support_credentials=True)
 
 
 if __name__=="__main__":
